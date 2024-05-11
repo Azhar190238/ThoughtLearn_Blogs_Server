c26c647d8e2b383764d92eebd255e832aa73f752
MD AZHARUDDIN <azhar73397@gmail.com>
2024-05-12 03:42:54 +0600

    Adding another database connection

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 // const jwt = require('jsonwebtoken');
 // const cookieParser = require('cookie-parser')
 // const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -33,6 +33,7 @@ async function run() {
 
     const blogsCollection = client.db('BlogsDB').collection('AllBlogs');
     const subscriberCollection = client.db('BlogsDB').collection('Subscriber');
+    const wishlistCollection = client.db('BlogsDB').collection('Wishlist')
 
 
 
@@ -43,6 +44,39 @@ async function run() {
                 const result = await cursor.toArray();
                 res.send(result);
             })
+           // specific data read for details
+
+            app.get('/addBlogs/:id' , async(req,res) => {
+               const id =req.params.id;
+               const query = { _id: new ObjectId(id)}
+               const result = await blogsCollection.findOne(query);
+               res.send(result);
+            })
+
+    // updated or put operation
+
+    app.put('/addBlogs/:id', async(req, res) =>{
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id)}
+        const options = {upsert:true};
+        const updatedBlog = req.body;
+        const blog ={
+            $set:{
+                title: updatedBlog.title,
+                short_description: updatedBlog.short_description,
+                long_description: updatedBlog.long_description,
+                category: updatedBlog.category,
+                photo: updatedBlog.photo
+               
+            }
+        }
+        const result = await blogsCollection.updateOne(filter,blog,options);
+        res.send(result);
+    })
+
+            
+
+     
     // data inserted from addBlogs
 
             app.post('/addBlogs', async(req,res)=>{
@@ -64,10 +98,27 @@ async function run() {
                 res.send(result);
           
             })
+   // data inserted for wishlist
+            app.post('/wishlist', async(req,res)=>{
+                const newSub = req.body;
+                console.log(newSub);
+                const result= await subscriberCollection.insertOne(newSub);
+                res.send(result);
+          
+            })
+
+
+    // //specific data read send to email
+
+    //   app.get('/wishlist/:id', async(req, res) =>{
+    //     const id= req.params.id;
+    //     const result = await blogsCollection.find({email:id}).toArray();
+    //     res.send(result);
+    // })
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
